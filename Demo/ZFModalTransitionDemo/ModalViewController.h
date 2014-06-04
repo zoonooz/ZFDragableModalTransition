dbede9b0fe76b2ea7f4ae256238e7419d220272a
Amornchai Kanokpullwad <amornchai.zoon@gmail.com>
2014-06-04 23:49:54 +0800

    added demo project

---
+++ Demo/ZFModalTransitionDemo/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  ZFModalTransitionDemo
+//
+//  Created by Amornchai Kanokpullwad on 6/4/14.
+//  Copyright (c) 2014 zoonref. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
---
+++ Demo/ZFModalTransitionDemo/ViewController.m
@@ -0,0 +1,62 @@
+//
+//  ViewController.m
+//  ZFModalTransitionDemo
+//
+//  Created by Amornchai Kanokpullwad on 6/4/14.
+//  Copyright (c) 2014 zoonref. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ModalViewController.h"
+#import "ZFModalTransitionAnimator.h"
+
+@interface ViewController ()
+@property BOOL dragable;
+@property (nonatomic, strong) ZFModalTransitionAnimator *animator;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.dragable = YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed:(UIButton *)sender
+{
+    ModalViewController *modalVC = [self.storyboard instantiateViewControllerWithIdentifier:@"ModalViewController"];
+    modalVC.modalPresentationStyle = UIModalPresentationCustom;
+    
+    self.animator = [[ZFModalTransitionAnimator alloc] initWithModalViewController:modalVC];
+    self.animator.dragable = self.dragable;
+    
+    NSString *title = [sender titleForState:UIControlStateNormal];
+    if ([title isEqualToString:@"Left"]) {
+        self.animator.direction = ZFModalTransitonDirectionLeft;
+    } else if ([title isEqualToString:@"Right"]) {
+        self.animator.direction = ZFModalTransitonDirectionRight;
+    } else {
+        self.animator.direction = ZFModalTransitonDirectionBottom;
+    }
+    
+    modalVC.transitioningDelegate = self.animator;
+    [self presentViewController:modalVC animated:YES completion:nil];
+}
+
+- (IBAction)dragableChanged:(UISwitch *)sender
+{
+    if (sender.on) {
+        self.dragable = YES;
+    } else {
+        self.dragable = NO;
+    }
+}
+
+@end
