c5b445c5fe04debdf68ced991a2ba3e39407082f
Albert Schulz <mail@halfco.de>
2015-05-21 07:56:24 +0200

    remove explicit fullscreen presentation mode

--- Demo/ZFModalTransitionDemo/ViewController.m
+++ Demo/ZFModalTransitionDemo/ViewController.m
@@ -32,7 +32,6 @@
 - (IBAction)buttonPressed:(UIButton *)sender
 {
     ModalViewController *modalVC = [self.storyboard instantiateViewControllerWithIdentifier:@"ModalViewController"];
-    modalVC.modalPresentationStyle = UIModalPresentationFullScreen;
     
     self.animator = [[ZFModalTransitionAnimator alloc] initWithModalViewController:modalVC];
     self.animator.dragable = self.dragable;
