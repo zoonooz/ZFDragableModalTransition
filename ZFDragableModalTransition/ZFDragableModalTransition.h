547a67dd07f020e78b7ffb7006573127258993d0
Denys Kotelovych <denys.kotelovych@gmail.com>
2015-12-20 22:20:51 +0200

    Added project + shared scheme;

---
+++ ZFDragableModalTransition/ZFDragableModalTransition.h
@@ -0,0 +1,19 @@
+//
+//  ZFDragableModalTransition.h
+//  ZFDragableModalTransition
+//
+//  Created by Denys Kotelovych on 12/20/15.
+//  Copyright © 2015 zoonref. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFDragableModalTransition.
+FOUNDATION_EXPORT double ZFDragableModalTransitionVersionNumber;
+
+//! Project version string for ZFDragableModalTransition.
+FOUNDATION_EXPORT const unsigned char ZFDragableModalTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFDragableModalTransition/PublicHeader.h>
+
+
